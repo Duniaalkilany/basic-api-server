3286fc7d54ebd4f4128b4e98f079cddac1d31518
Duniaalkilany <duniaalkilany908@gmail.com>
2021-08-12 12:53:25 +0300

    testing

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,19 @@
 'use strict';
-const POSTGRES_URI = process.env.NODEENV =="test" ? 'sqlite:memory ': "postgres://username:pgpassword@db:5432/dunia" ;
+const POSTGRES_URI =  "postgres://dunia:pgpassword@db:5432/dunia" ;
 
 const { Sequelize, DataTypes } = require('sequelize');
 const FoodModel= require('./food');
 const  ClothesModel  = require('./clothes');
 
-let sequelize = new Sequelize(POSTGRES_URI,{});
+// let sequelize = new Sequelize(POSTGRES_URI,{});
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  dialect: 'postgres',
+  host: "localhost",
+  port: 5433,
+});
+
+
 // lets define our Schema
 module.exports = {
       Clothes: ClothesModel(sequelize, DataTypes),
