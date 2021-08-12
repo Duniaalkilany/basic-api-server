747241db81d06b4a4eea67add2914da4d2dd760e
Duniaalkilany <duniaalkilany908@gmail.com>
2021-08-12 13:24:08 +0300

    testing

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,15 @@
 'use strict';
-const POSTGRES_URI ="postgres://localhost:5432/dunia"
+const POSTGRES_URI ="postgres://localhost:5433/dunia"
 
 const { Sequelize, DataTypes } = require('sequelize');
 const FoodModel= require('./food');
 const  ClothesModel  = require('./clothes');
 
+const db = new Sequelize('dunia', 'postgres', `${process.env.DB_PASSWORD}`, {
+    host: 'localhost',
+    dialect: 'postgres',
+    port:5433,
+} );
 let sequelize = new Sequelize(POSTGRES_URI,{});
 
 
